3699e0f38e3361614df13f4257daf31ce4b3de5c
xzadolberx <exploited_man@bk.ru>
2019-02-04 00:36:55 +0300

    "Сконструировал двух собак, поиграл с конструкторами"

--- Doge.java
+++ Doge.java
@@ -1,8 +1,28 @@
 
 public class Doge extends Animal {
+	String color;
+	String name;
+	double height;
+	double weight;
+
+	static boolean Kot;
+
 	public Doge() {
 		setLegs(4);
 	    setVoice("Bark!");
 	}
-	
+
+	public Doge(int legs, String voice, String color, String name, double height, double weight, boolean Kot) {
+		this.legs = legs;
+		this.voice = voice;
+		this.color = color;
+		this.name = name;
+		this.height = height;
+		this.weight = weight;
+		this.Kot = Kot;
+	}
+
+	Doge Mukhtar = new Doge (4, "GAF!", "yellow", "Mukhtar", 0.65, 37.2, false);
+	Doge Kotopes = new Doge (4, "“еперь у мен€ весь мех в креме, и уши слиплись!", 
+								"yellow and brown", "Kotopes", 0.40, 20.2, true);
 }
