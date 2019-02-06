eb2dee7575d7f5444d53ab70471d292d453816a5
Timur Iskhakov <xzadolberx@users.noreply.github.com>
2019-02-06 18:28:23 +0300

    Поправил наименования объектов

--- Animal.java
+++ Animal.java
@@ -1,22 +1,23 @@
 public class Animal {
 	public int legs;
 
-	public int setLegs (int legs)
+	public int setLegs(int legs)
 	{
 		return legs;
 	}
 	
 	public String voice;
 
-	public String setVoice (String voice)
+	public String setVoice(String voice)
 	{
 		this.voice = voice;
 		//System.out.println (voice);
 		return voice;
 	}
 
-	public void getVoice() {
-		System.out.println (voice);
+	public void getVoice() 
+	{
+		System.out.println(voice);
 	}
 
 	public static void main(String... args) {
@@ -24,8 +25,8 @@ public class Animal {
 		Animal doge = new Doge();
 		Animal animal = new Animal();
 
-		Animal coteWithConstructor = new Cote ("Дай жрать!",4);
-		Cote chernobyl_cote = new Cote ("Проходи сталкер!", 6);
+		Animal coteWithConstructor = new Cote("Дай жрать!",4);
+		Cote chernobyl_cote = new Cote("Проходи сталкер!", 6);
 
 		System.out.println(cote.legs);
 		System.out.println(doge.legs);
@@ -47,7 +48,5 @@ public class Animal {
 		coteWhatBeenAnimal.climb ("Дерево", 3);
 	}
 
-	private void tmp(Animal a) {
-
-	}
+	private void tmp(Animal a) {}
 }
